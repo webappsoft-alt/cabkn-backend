ede81c8865278b7168438f324479f85bb0ca509e
Jabbar Ahmad <jabbarahmad540@gmail.com>
2024-11-28 14:57:33 +0500

    comp

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const categoriesController = require('../controllers/categoriesController');
-const admin = require('../middleware/admin');
+// const admin = require('../middleware/admin');
 const auth = require('../middleware/auth');
 
-router.post('/create', [auth,admin],categoriesController.create);
-router.get('/admin-all',[auth,admin], categoriesController.getCategories);
-router.get('/admin/:id', [auth,admin], categoriesController.getAllCategories);
-router.put('/edit/:id',[auth,admin],  categoriesController.editCategories);
+router.post('/create', auth,categoriesController.create);
+router.get('/admin-all',auth, categoriesController.getCategories);
+router.get('/admin/:id', auth, categoriesController.getAllCategories);
+router.put('/edit/:id',auth,  categoriesController.editCategories);
 router.get('/all/:id?', categoriesController.getAllCustomerCategories);
-router.put('/:status/:id',[auth,admin], categoriesController.deactivateCategries);
+router.put('/:status/:id',auth, categoriesController.deactivateCategries);
 
 module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -4,7 +4,7 @@ const auth = require('../routes/auth');
 const users = require('../routes/users');
 const uploadImages = require('../routes/uploadImages');
 // const postRoutes = require('../routes/postRoutes');
-// const categoryRoute = require('../routes/categoryRoute');
+const categoryRoute = require('../routes/categoryRoute');
 // const couponRoute = require('../routes/couponRoute');
 // const jadeRoutes = require('../routes/jadePaymentRoutes');
 // const resellRoutes = require('../routes/resellRoutes');
@@ -19,7 +19,7 @@ module.exports = function (app) {
   app.use('/api/auth', auth);
   app.use('/api/users', users);
   app.use('/api/image', uploadImages);
-  // app.use('/api/category', categoryRoute);
+  app.use('/api/category', categoryRoute);
   // app.use('/api/banner', BannerRoute);
   // app.use('/api/coupon', couponRoute);
   // app.use('/api/event',  postRoutes);
