27abb7cd33fa025154c639eca6c1db6e8c85b107
Jabbar Ahmad <jabbarahmad540@gmail.com>
2024-12-10 16:14:24 +0500

    asd

--- routes/users.js
+++ routes/users.js
@@ -398,7 +398,7 @@ router.put("/update-location", auth, async (req, res) => {
   res.send({ success: true, message: "User updated successfully", user });
 });
 
-router.get('/nearby/:id',[auth,admin], async (req, res) => {
+router.get('/nearby/:id',auth, async (req, res) => {
   const lastId = parseInt(req.params.id)||1;
   const { lat, lng } = req.body;
 
