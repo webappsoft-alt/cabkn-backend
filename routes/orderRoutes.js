8985d40a9a772df67d59a63bb721d0abd32596fc
Jabbar Ahmad <jabbarahmad540@gmail.com>
2024-12-11 17:46:01 +0500

    asd

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -9,7 +9,7 @@ router.get('/offer/detail/:id', orderController.getofferDetails);
 router.get('/detail/:id', orderController.getOrderDetails);
 router.post('/rider/:status/:id?', orderController.getAllEmployeeApplication);
 router.post('/customer/:status/:id?', orderController.getAllSellerApplication);
-router.post('/admin/:status/:id', orderController.AdminRides);
+router.post('/admin/:status/:id',admin, orderController.AdminRides);
 router.put('/admin/update-purchases/:id',admin, orderController.updatePurchasePaymentByAdmin);
 router.put('/customer/pay/:id', orderController.updatePurchasePaymentByCustomer);
 router.put('/rider/approve/:id', orderController.updateApproveByRider);
