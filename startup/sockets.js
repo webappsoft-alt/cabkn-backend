52b090c78663d5a5b046887cd377b6cdeac3ed29
arslan0044 <72161665+arslan0044@users.noreply.github.com>
2025-06-04 16:56:15 +0500

    Deploying to VPS

--- startup/sockets.js
+++ startup/sockets.js
@@ -331,9 +331,9 @@ module.exports = function (server, app) {
           };
         }
 
-        let userIds = await User.find(query).select("name fcmtoken").lean();
+        let userIds = await User.find(query).select("name type fcmtoken").lean();
         //  const users = await getUsersInRadius(start_lng, start_lat, 5, address)
-        console.log("userIds====>>", userIds.map((item) =>{ item._id, item.type}));
+        console.log("userIds====>>", userIds.map((item) =>({ _id: item._id, type: item.type})));
 
         if (subcatId) {
           const subCat = await WebSubCategories.findById(subcatId);
