42b669bfd4f720d3b29007bb43b95763bafa0568
arslan0044 <72161665+arslan0044@users.noreply.github.com>
2025-09-10 18:17:20 +0500

    Deploying to VPS

--- startup/sockets.js
+++ startup/sockets.js
@@ -777,7 +777,7 @@ module.exports = function (server, app) {
               userType: request.user.type,
               success: true,
               title: "New Request",
-              message: `New request has been created by ${sender.name}`,
+              message: `New request has been created by Admin`,
             });
           });
         } else {
